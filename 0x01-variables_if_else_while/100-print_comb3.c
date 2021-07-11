5af581a264866d9be4893f497d2fa1ecafd7b179
BimbolaA <abimbolaabiri@gmail.com>
2021-07-11 06:54:59 -0700

    Advance Task

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ *main - print out all the numbers 00 and 99
+ *with no two digits being the same
+ *return: 0
+ */
+int main(void)
+{
+int i, j;
+
+for (i = 48; i < 58; i++)
+{
+for (j = i; j < 58; j++)
+{
+if (i == j)
+{
+continue;
+}
+
+putchar(i);
+putchar(j);
+
+if (i == 56 && j == 57)
+{
+break;
+}
+else
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ *main - print a number pair from 00-99 without repetition
+ *Return: 0
+ */
+int main(void)
+{
+int j, i;
+
+for (j = 0; j <= 99; j++)
+{
+for (i = j; I <= 99; i++)
+{
+if (i != j)
+{
+
+putchar(j / 10 + 48);
+putchar(j % 10 + 48);
+putchar(' ');
+putchar(i / 10 + 48);
+
+if (j * 100 + i != 9899)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
