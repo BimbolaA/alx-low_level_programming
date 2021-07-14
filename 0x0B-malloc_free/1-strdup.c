a27c962a97a03ed37d7edecd83bb1969e577e31f
BimbolaA <abimbolaabiri@gmail.com>
2021-07-14 10:41:39 -0700

    Malloc Free

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * _strdup - returns a pointer to a newly allocated space
+ * containing a copy of given parameters
+ * @str: string to duplicate
+ * Return: pointer to the duplicate string
+ */
+
+char *_strdup(char *str)
+{
+char *str_copy;
+int i = 0, size = 0;
+
+if (str == NULL) /* validate string input */
+return (NULL);
+
+while (*(str + i))
+size++, i++; /* add null terminator to size */
+size++;
+
+str_copy = maloc(sizeof(char) * size); /* allocate memory */
+
+if (str-copy == NULL) /* validate memory */
+return (NULL);
+
+i = 0;
+while (i < size)
+{
+*(str_copy + i) = *(str + i);
+i++;
+}
+
+return (str_copy);
+}
