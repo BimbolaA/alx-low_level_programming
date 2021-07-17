1ef8268329d1e907a3a6b9ba2748b2bcd4c58a99
BimbolaA <abimbolaabiri@gmail.com>
2021-07-17 10:29:43 -0700

    Nested Loops

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ *_islower - check for lowercase characters
+ *Return: return 1 for lowercase and 0 otherwise
+ *@c: the character to print
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+return;
+else
+return (0);
+}
