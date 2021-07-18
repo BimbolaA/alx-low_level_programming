1c93af46d0bfcdf64cfa46cf2067a32285dc62ea
BimbolaA <abimbolaabiri@gmail.com>
2021-07-18 12:28:40 -0700

    Further commit

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,7 +4,8 @@
  * _memset - fills memory with constant byte
  * @i: memory area
  * @k: constant byte
- * @j: bytes of memory area i
+ * @j: bytes of memory area
+ * Return: pointer to memory area i
  */
 char *_memset(char *i, char k, unsigned int j)
 {
---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * memcpy - copies memory area
+ * @dest: destination string
+ * @src: source string
+ * @d: number of bytes to be copied
+ * Return: pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n);
+{
+char *ptr = dest;
+
+while (d--)
+*dest++ = *src++;
+return (ptr);
+}
