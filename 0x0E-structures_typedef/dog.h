c7f672fd4dd566930242b2ef6ae6c772e456cc9c
BimbolaA <abimbolaabiri@gmail.com>
2021-07-20 03:39:11 -0700

    struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -21,7 +21,7 @@ int _strlen(chat *s);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
-void free-dog(dog_t *d);
+void free_dog(dog_t *d);
 char *_strdup(char *str);
 
 #endif
