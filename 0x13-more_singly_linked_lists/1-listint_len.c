b9bf3f65aa228329c687507cf5ce59ac3eaa1ebf
BimbolaA <abimbolaabiri@gmail.com>
2021-08-03 16:00:52 -0700

    further commit

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "list.h"
+
+/**
+ * listint_len - rturns the number of elements
+ * @h: pointer to head of list
+ * Return: number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+register size_t count = 0;
+
+while (h)
+{
+h = h->next;
+count++;
+}
+return (count);
+}
