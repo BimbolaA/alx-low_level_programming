9bbfa9a1dd12c49981a6dc69f5b9bb30fd7ed21b
Eze Goodluck <ezegoodluck78@gmail.com>
2023-02-17 08:44:05 -0800

    commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,3 @@
-/*
- * File: 0-positive_or_negative.c
- * Auth: Brennan D Baraban
- */
 
 #include <stdlib.h>
 #include <time.h>
@@ -22,7 +18,7 @@ int main(void)
 
 	if (n > 0)
 		printf("%d is positive\n", n);
-	else if (n < 0)
+	else if (n == 0)
 		printf("%d is negative\n", n);
 	else
 		printf("%d is zero\n", n);
