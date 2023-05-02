2104739c72ba7218994b1108bf6b8799c06ff265
BimbolaA <abimbolaabiri@gmail.com>
2023-05-02 11:28:02 -0700

    Correction

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,23 +1,20 @@
 #include "lists.h"
+
 /**
- * listint_len - get the number of nodes in a list
- * @h: head of the count
+ * listint_len - counts the number of nodes in a linked list
+ * @h: head of the list
  *
- * Return: the amount of nodes
+ * Return: the number of elements
  */
-
 size_t listint_len(const listint_t *h)
-
 {
+	const listint_t *cursor = h;
 	size_t count = 0;
 
-	while (h)
+	while (cursor != NULL)
 	{
-		count++;
-
-		h = h->next;
+		count += 1;
+		cursor = cursor->next;
 	}
-
 	return (count);
-
 }
