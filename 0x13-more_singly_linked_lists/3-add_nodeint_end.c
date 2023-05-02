e1a816bb3fa334cf5aa6d05bc75a9b7029d7f4bf
BimbolaA <abimbolaabiri@gmail.com>
2023-05-02 11:38:21 -0700

    fix

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,31 +1,32 @@
 #include "lists.h"
-listint_t *create_nodes(const int n);
 
 /**
- * add_nodeint_end - Adds a node at the end of the list
- * @head: pointer to the current place in the list
- * @n: int to add to the list
- * Retrun: pointer to original position in the list
+ * add_nodeint_end - adds a node to the end of a linked list
+ * @head: pointer to the head of the list
+ * @n: number to be used as content
+ *
+ * Return: address of the newly added node
  */
-listint_t *add_nodeint_end(listint_t **head, cont int n)
+listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *new;
-	listint_t *tmp;
-		new = malloc(sizeof(listint_t));
+	listint_t *new_node;
+	listint_t *cursor = *head;
 
-	if (!new)
+	new_node = malloc(sizeof(listint_t));
+	if (new_node != NULL)
+	{
+		new_node->n = n;
+		new_node->next = NULL;
+	}
+	else
 		return (NULL);
-	new->n = n;
-	new->next = NULL
-
-		temp = *head;
-
-	if (head == NULL)
-		return (NULL);
-}
-
-while (temp->next)
-	temp = temp->next;
-	temp->next = new;
-	return (new);
+	if (cursor != NULL)
+	{
+		while (cursor->next != NULL)
+			cursor = cursor->next;
+		cursor->next = new_node;
+	}
+	else
+		*head = new_node;
+	return (new_node);
 }
--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,20 +1,19 @@
 #include "lists.h"
+
 /**
- * free_listint - frees a list node
- * @head: pointer to the first element of the list
+ * free_listint - frees a linked list
+ * @head: head of the list
+ *
  * Return: void
  */
 void free_listint(listint_t *head)
 {
-	listint_t *tmp;
+	listint_t *temp;
 
-	if (head)
-		return;
-	while (head->next != NULL)
+	while (head != NULL)
 	{
-		tmp = head->next;
-		free(head);
-		head = tmp;
+		temp = head;
+		head = head->next;
+		free(temp);
 	}
-	free(head);
 }
